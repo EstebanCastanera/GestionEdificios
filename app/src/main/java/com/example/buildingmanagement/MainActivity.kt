c36074024c5006bc20bc84176b5f5919d0cc655d
Esteban Castañera <leoquen@hotmail.com>
2022-04-02 02:45:19 -0300

    InicioProyecto

--- app/src/main/java/com/example/buildingmanagement/MainActivity.kt
+++ app/src/main/java/com/example/buildingmanagement/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.buildingmanagement.ui.theme.BuildingManagementTheme
 
+
+// Prueba
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
